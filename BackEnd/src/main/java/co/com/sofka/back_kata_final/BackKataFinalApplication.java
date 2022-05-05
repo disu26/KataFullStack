d42e35ac5f24f68eb7301a5d510873e06426fe21
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-05 14:19:38 -0500

    Creación de la estructura básica del back con spring.

---
+++ BackEnd/src/main/java/co/com/sofka/back_kata_final/BackKataFinalApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.back_kata_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackKataFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackKataFinalApplication.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/co/com/sofka/back_kata_final/BackKataFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.back_kata_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackKataFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
